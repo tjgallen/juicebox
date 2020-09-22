746d778e3e187286a008e3e0fff460f4b4020e06
Tammy Allen <tjgallen@aol.com>
2020-09-21 21:33:22 -0700

    Juicebox Part 2, Day 1

--- api/tags.js
+++ api/tags.js
@@ -2,6 +2,11 @@ const express = require('express');
 const tagsRouter = express.Router();
 const { getAllTags } = require('../db');
 
+tagsRouter.use((req, res, next) => {
+    console.log("A request is being made to /tags");
+  
+    next(); // THIS IS DIFFERENT
+  });
 
 tagsRouter.get('/', async (req, res) => {
     const tags = await getAllTags();
