00318963bea3be5b3b266c827e75d054f93a1ff8
Tammy Allen <tjgallen@aol.com>
2020-09-26 12:42:18 -0700

    Juicebox, Part 2, Day 3

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 const { Client } = require('pg') // imports the pg module
 
-const client = new Client('postgres://localhost:5432/juicebox-dev');
-
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 /**
  * USER Methods
  */
@@ -202,7 +201,7 @@ async function getPostById(postId) {
         message: "Could not find a post with that postId"
       };
     }
-    
+
     const { rows: tags } = await client.query(`
       SELECT tags.*
       FROM tags
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 const express = require('express');
 const server = express();
 const chalk = require('chalk');
